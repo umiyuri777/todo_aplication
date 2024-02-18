87f46061c2fba11e8f752855993daf18b96821cc
umiyuri777 <yuseiai15@gmail.com>
2024-02-18 17:01:10 +0900

    タブ分けして表示した (タスクのフィルタは未実装)

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 void main() {
   runApp(
     const ProviderScope(
-      child: const MyApp()
+      child: MyApp()
     )
   );
 }
@@ -23,89 +23,87 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  MyHomePage(),
+      home:  const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends HookConsumerWidget {
+  const MyHomePage({super.key});
+
 
   @override
   Widget build(BuildContext context, WidgetRef ref){
     // final todo = useState<List<String>>([]);
     // final TextEditingController _controller = useTextEditingController();
     final todolist = ref.watch(todolistProvider);
-    final tabController = useTabController(initialLength: 2);
+    // final tabController = useTabController(initialLength: 2);
     
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todoリスト'),
-        backgroundColor: Colors.blue,
-        //tabbarを設定
-        bottom: TabBar(
-          controller: tabController,
-          tabs: const [
-            Tab(text: "未完了",),
-            Tab(text: "完了済",)
-          ],  
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => NewNoteCreate()),
-          ).then((value) {
-            ref.read(todolistProvider.notifier).add(value);
-          });
-        },
-        child: const Icon(Icons.add)  //追加ボタン
-      ),
-      body: Column(
-        children: [
-          // TextFormField(
-          //   controller: _controller,
-          //   keyboardType: TextInputType.multiline,
-          //   maxLines: null,
-          //
-          //   onFieldSubmitted:(value) {
-          //     ref.read(todolistProvider.notifier).add(_controller.value.text);
-          //   },
-          //   decoration: InputDecoration(
-          //     border: InputBorder.none,
-          //     hintText: 'タスクを入力してください',
-          //   ),
-          // ),
-          // ElevatedButton(
-          //   onPressed: () {
-          //     ref.read(todolistProvider.notifier).add(_controller.value.text);
-          //   }, 
-          //   child: const Text('追加')
-          // ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: todolist.length,
-            itemBuilder: (context, index){
-              return Card(
-                child: ListTile(
-                  title: Text('${todolist[index]}'),
-                  trailing: IconButton(
-                    onPressed: () {
-                      ref.read(todolistProvider.notifier).remove(todolist[index]);
-                    },
-                    icon: Icon(Icons.delete)
-                  ),
-                )
-              );
-            }
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Todoリスト'),
+            backgroundColor: Colors.blue,
+            //tabbarを設定
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: "未完了", icon: Icon(Icons.star),),
+                Tab(text: "完了済", icon: Icon(Icons.done),)
+              ],  
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const NewNoteCreate()),
+              ).then((value) {
+                ref.read(todolistProvider.notifier).add(value);
+              });
+            },
+            child: const Icon(Icons.add)  //追加ボタン
+          ),
+          body: TabBarView(
+            children: [
+              _buildListView(todolist, ref),
+              const Center(
+                child: Text(
+                'mikanのタスク', 
+                style: TextStyle(fontSize: 20))
+              ),
+            ]
           )
-        ],
+        ),
       ),
     );
   }
 }
 
+Widget _buildListView(todolist, WidgetRef ref){
+  return  ListView.builder(
+    shrinkWrap: true,
+    itemCount: todolist.length,
+    itemBuilder: (context, index){
+      return Card(
+        child: ListTile(
+          title: Text(todolist[index]),
+          trailing: IconButton(
+            onPressed: () {
+              ref.read(todolistProvider.notifier).remove(todolist[index]);
+            },
+            icon: const Icon(Icons.delete)
+          ),
+        )
+      );
+    }
+  );
+}
+
 class NewNoteCreate extends HookConsumerWidget {
+  const NewNoteCreate({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final TextEditingController _controller = useTextEditingController();
